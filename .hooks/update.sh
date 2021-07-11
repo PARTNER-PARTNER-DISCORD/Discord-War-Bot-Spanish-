75f64501ed11149dc84bac584d1341845783966b
XtoMHA96 <xtomanuel.gamer@gmail.com>
2021-07-11 11:29:13 +0100

    [14/10/2020] Update npm packages, uninstall youtube-dl in update.sh

--- .hooks/update.sh
+++ .hooks/update.sh
@@ -2,7 +2,7 @@
 if grep -q " git@github.com:" .git/config; then
   echo "Attempting update from GIT via SSH"
   #ssh-agent bash -c "ssh-add ~/.ssh/sb_id_rsa_nopass && git fetch --all && git reset --hard origin/master && git submodule update && npm i --production && npm i youtube-dl"; kill $SSH_AGENT_PID
-  ssh-agent bash -c "ssh-add ~/.ssh/sb_id_rsa_nopass && git fetch --all && git reset --hard origin/master && git submodule update && npm i --production"; kill $SSH_AGENT_PID
+  ssh-agent bash -c "ssh-add ~/.ssh/sb_id_rsa_nopass && git fetch --all && git reset --hard origin/master && git submodule update && npm i --production  && npm remove youtube-dl"; kill $SSH_AGENT_PID
 else
   echo "Attempting update from GIT via HTTP"
   git fetch --all && git reset --hard origin/master && git submodule update && npm i --production && npm i youtube-dl
