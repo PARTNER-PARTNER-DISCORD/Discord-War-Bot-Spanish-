eb510745e2d3d109d19b10dbeb181863e1908bd2
XtoMHA96 <xtomanuel.gamer@gmail.com>
2023-07-17 12:32:06 +0100

    Test fixing

--- .hooks/install-hooks.sh
+++ .hooks/install-hooks.sh
@@ -5,9 +5,9 @@ HOOK_DIR="$REPO_ROOT/.git/hooks"
 
 for hook in $HOOK_NAMES; do
   # If the hook already exists, is executable, and is not a symlink.
-  if [ ! -h "$HOOK_DIR/$hook" -a -x "$HOOK_DIR/$hook" ]; then
-      mv "$HOOK_DIR/$hook" "$HOOK_DIR/$hook.local"
+  if [ ! -h $HOOK_DIR/$hook -a -x $HOOK_DIR/$hook ]; then
+      mv $HOOK_DIR/$hook $HOOK_DIR/$hook.local
   fi
   # create the symlink, overwriting the file if it exists
-  ln -s -f "$REPO_ROOT/.hooks/$hook" "$HOOK_DIR/$hook"
+  ln -s -f $REPO_ROOT/.hooks/$hook $HOOK_DIR/$hook
 done
